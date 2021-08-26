46a38c429cce2d777bca9a8a6630573943d16809
Ali Ahsani <70918831+Khanali00@users.noreply.github.com>
2021-08-27 01:27:35 +0200

    Exercises

--- Exercises/Problem.py
+++ Exercises/Problem.py
@@ -1,3 +1,24 @@
+
+import math
+#question nr 1a
+a = 3
+b= 4
+answer=math.sqrt((a**2) + (b**2))
+
+print(f"The hyp is {answer} length unit")
+
+import math
+#question nr 1b
+
+c = 7
+a = 5
+answer = round(math.sqrt ((c**2) - (a**2)), 2)
+
+print (f"The hypothenuse is {answer} length unit")
+
+
+
+
 #question nr 2
 
 days = 365
